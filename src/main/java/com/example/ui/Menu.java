d741b4724b94639f09798324ff343c489e177567
QingTian <qingtian63910@proton.me>
2024-06-28 20:00:24 +0700

    feat: created a basic Menu class.

---
+++ src/main/java/com/example/ui/Menu.java
@@ -0,0 +1,62 @@
+package com.example.ui;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public final class Menu {
+    private Menu() {}
+
+    public static <E> void listOptions(ArrayList<E> options) {
+        if (options.isEmpty()) {
+            return;
+        }
+
+        for (int i = 0; i < options.size(); i++) {
+            System.out.printf(
+                    "%d. %s\n%n",
+                    i + 1,
+                    options.get(i).toString()
+            );
+        }
+
+        System.out.println();
+        System.out.printf("Please choose an option 1..%d:", options.size());
+    }
+
+    public static <E> int int_getChoice(ArrayList<E> options) {
+        if (options.isEmpty()) {
+            return -1;
+        }
+
+        listOptions(options);
+        Scanner sc = new Scanner(System.in);
+        int choice = -1;
+
+        while (choice == -1) {
+            try {
+                choice = sc.nextInt();
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid choice! Try again!");
+                sc.nextLine();
+            }
+        }
+        return choice;
+    }
+
+    public static <E> E ref_getChoice(ArrayList<E> options) {
+        if (options.isEmpty()) {
+            return null;
+        }
+
+        listOptions(options);
+        int choice;
+
+        do {
+            choice = int_getChoice(options);
+        } while (choice < 0 || choice > options.size());
+
+        return options.get(choice - 1);
+    }
+}
