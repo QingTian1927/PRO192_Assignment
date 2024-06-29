2bb137c6e22fb1312fb9502da87693bf574c37ae
QingTian <qingtian63910@proton.me>
2024-06-29 10:34:40 +0700

    feat: updated the CarList class.
    * Added some additional methods to list, add, update and remove cars in the CarList class.

--- src/main/java/com/example/models/CarList.java
+++ src/main/java/com/example/models/CarList.java
@@ -1,10 +1,14 @@
 package com.example.models;
 
+import com.example.ui.Menu;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class CarList {
-    private BrandList brandList;
-    private ArrayList<Car> carList;
+    private final BrandList brandList;
+    private final ArrayList<Car> carList;
 
     public CarList(BrandList brandList) {
         this.carList = new ArrayList<Car>();
@@ -26,6 +30,18 @@ public class CarList {
             result.append(car.screenString()).append('\n');
         }
         return result.toString();
+    }
+
+    public void listCars() {
+        ArrayList<Car> clonedCars = new ArrayList<Car>(this.carList.size());
+        for (Car car : this.carList) {
+            clonedCars.add(car.getCopy());
+        }
+        Collections.sort(clonedCars);
+
+        for (Car car : clonedCars) {
+            System.out.println(car.screenString());
+        }
     }
 
     public int searchId(String carId) {
@@ -55,7 +71,146 @@ public class CarList {
         return -1;
     }
 
-    public void addCar() {
+    public void printBasedBrandName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a part of brand name: ");
+        String query = sc.nextLine();
 
+        int count = 0;
+        for (Car car : this.carList) {
+            if (car.getBrand().getBrandName().contains(query)) {
+                System.out.println(car.screenString());
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            System.out.println("No car is detected!");
+        }
+    }
+
+    private Brand inputBrand() {
+        System.out.println("Choose a car brand from the following list:\n");
+        Brand brand = (Brand) Menu.ref_getChoice(this.brandList.getBrandList());
+        System.out.println();
+        return brand;
+    }
+
+    private String inputColor() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the car color: ");
+        String color = sc.nextLine();
+        System.out.println();
+        return color;
+    }
+
+    private String inputFrameId() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a new frame ID: ");
+        int searchResult = 0;
+        String frameId = null;
+
+        do {
+            frameId = sc.nextLine();
+            if (!Car.isValidFrameId(frameId)) {
+                System.out.println("Frame ID must be in the F0000 format!");
+                continue;
+            }
+
+            searchResult = this.searchFrame(frameId);
+            if (searchResult != -1) {
+                System.out.println("Frame ID must not be duplicated!");
+            }
+        } while (searchResult != -1);
+        System.out.println();
+
+        return frameId;
+    }
+
+    private String inputEngineId() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a new engine ID: ");
+        int searchResult = 0;
+        String engineId = null;
+
+        do {
+            engineId = sc.nextLine();
+            if (!Car.isValidEngineId(engineId)) {
+                System.out.println("Engine ID must be in the E0000 format!");
+                continue;
+            }
+
+            searchResult = this.searchEngine(engineId);
+            if (searchResult != -1) {
+                System.out.println("Engine ID must not be duplicated!");
+            }
+        } while (searchResult != -1);
+        System.out.println();
+
+        return engineId;
+    }
+
+    public void addCar() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a new car ID: ");
+        int searchResult = 0;
+        String carId = null;
+
+        do {
+            carId = sc.nextLine();
+            searchResult = this.searchId(carId);
+
+            if (searchResult != -1) {
+                System.out.println("Car ID must not be duplicated!");
+            }
+        } while (searchResult != -1);
+        System.out.println();
+
+        Brand brand = inputBrand();
+        String color = inputColor();
+        String frameId = inputFrameId();
+        String engineId = inputEngineId();
+
+        this.carList.add(new Car(carId, brand, color, frameId, engineId));
+    }
+
+    public boolean removeCar() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the car ID: ");
+        String carId = sc.nextLine();
+
+        int index = this.searchId(carId);
+        if (index == -1) {
+            System.out.println("Not found!");
+            return false;
+        }
+
+        this.carList.remove(index);
+        return true;
+    }
+
+    public boolean updateCar() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the car ID: ");
+        String carId = sc.nextLine();
+
+        int index = this.searchId(carId);
+        if (index == -1) {
+            System.out.println("Not found!");
+            return false;
+        }
+
+        Brand brand = inputBrand();
+        String color = inputColor();
+        String frameId = inputFrameId();
+        String engineId = inputEngineId();
+
+        Car updatedCar = this.carList.get(index);
+        updatedCar.setBrand(brand);
+        updatedCar.setColor(color);
+        updatedCar.setFrameId(frameId);
+        updatedCar.setEngineId(engineId);
+
+        return true;
     }
 }
