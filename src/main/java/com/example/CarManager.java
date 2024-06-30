9d72e359122291f5735b01b61a200a616acfc7bb
QingTian <qingtian63910@proton.me>
2024-06-30 22:55:50 +0700

    feat: implemented some extra features in CarManager.

--- src/main/java/com/example/CarManager.java
+++ src/main/java/com/example/CarManager.java
@@ -5,36 +5,45 @@ import com.example.models.CarList;
 import com.example.ui.UserInterface;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class CarManager {
+    private static final String BRANDS_SOURCE_FILE = "resources/brands.txt";
+    private static final String CARS_SOURCE_FILE = "resources/cars.txt";
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         BrandList brandList = new BrandList();
         CarList carList = new CarList(brandList);
-        UserInterface.clearScreen();
-
         System.out.println(System.getProperty("user.dir"));
+
         try {
-            brandList.readFromFile("resources/brands.txt");
+            brandList.readFromFile(CarManager.BRANDS_SOURCE_FILE);
         } catch (IOException e) {
             System.out.println("FATAL: Failed to read brands.txt");
-            System.out.println("FATAL: " + e);
+            System.out.println("CAUSE: " + e);
             System.exit(1);
         }
 
         try {
-            carList.readFromFile("resources/cars.txt");
+            carList.readFromFile(CarManager.CARS_SOURCE_FILE);
         } catch (IOException e) {
             System.out.println("FATAL: Failed to read cars.txt");
-            System.out.println("FATAL: " + e);
+            System.out.println("CAUSE: " + e);
             System.exit(1);
         }
 
+        String message = null;
         boolean isRunning = true;
+
         while (isRunning) {
+            UserInterface.clearScreen();
             UserInterface.printTitleCard();
+            if (message != null) {
+                UserInterface.printMessage(message);
+                message = null;
+            }
+
             int choice = UserInterface.mainMenuOptions();
+
 
             switch (choice) {
                 case 1:
@@ -46,14 +55,23 @@ public class CarManager {
                 case 0:
                     isRunning = false;
                     break;
+                default:
+                    message = "Invalid choice! Try again!";
             }
         }
     }
 
     public static void manageBrands(BrandList brandList) {
+        String message = null;
+
         while (true) {
             UserInterface.clearScreen();
             UserInterface.printTitleCard();
+            if (message != null) {
+                UserInterface.printMessage(message);
+                message = null;
+            }
+
             int choice = UserInterface.brandMenuOptions();
             System.out.println();
 
@@ -75,18 +93,51 @@ public class CarManager {
                     UserInterface.pressEnterToContinue();
                     break;
                 case 5:
+                    saveBrandsHandler(brandList);
                     break;
                 case 0:
+                    if (!brandList.isModified()) {
+                        return;
+                    }
+
+                    System.out.println("You have unsaved changes!");
+                    boolean goSaveFile = UserInterface.yesNoQuestion("Do you want to save them? [y/n]: ");
+                    if (goSaveFile) {
+                        saveBrandsHandler(brandList);
+                    }
                     return;
+                default:
+                    message = "Invalid choice! Try again!";
             }
         }
     }
 
+    public static void saveBrandsHandler(BrandList brandList) {
+        System.out.printf("Saving contents to %s...\n", CarManager.BRANDS_SOURCE_FILE);
+        try {
+            brandList.saveToFile(CarManager.BRANDS_SOURCE_FILE);
+            System.out.println("Successfully saved contents to file.\n");
+            UserInterface.pressEnterToContinue();
+        } catch (IOException e) {
+            System.out.println("FATAL: Failed to saved contents to file!");
+            System.out.println("CAUSE: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+
     public static void manageCars(CarList carList) {
+        String message = null;
+
         while (true) {
             UserInterface.clearScreen();
             UserInterface.printTitleCard();
-            int choice = UserInterface.brandMenuOptions();
+            if (message != null) {
+                UserInterface.printMessage(message);
+                message = null;
+            }
+
+            int choice = UserInterface.carMenuOptions();
+            System.out.println();
 
             switch (choice) {
                 case 1:
@@ -110,10 +161,35 @@ public class CarManager {
                     UserInterface.pressEnterToContinue();
                     break;
                 case 6:
+                    saveCarsHandler(carList);
                     break;
                 case 0:
+                    if (!carList.isModified()) {
+                        return;
+                    }
+
+                    System.out.println("You have unsaved changes!");
+                    boolean goSaveFile = UserInterface.yesNoQuestion("Do you want to save them? [y/n]: ");
+                    if (goSaveFile) {
+                        saveCarsHandler(carList);
+                    }
                     return;
+                default:
+                    message = "Invalid choice! Try again!";
             }
+        }
+    }
+
+    public static void saveCarsHandler(CarList carList) {
+        System.out.printf("Saving contents to %s...\n", CarManager.CARS_SOURCE_FILE);
+        try {
+            carList.saveToFile(CarManager.CARS_SOURCE_FILE);
+            System.out.println("Successfully saved contents to file.\n");
+            UserInterface.pressEnterToContinue();
+        } catch (IOException e) {
+            System.out.println("FATAL: Failed to saved contents to file!");
+            System.out.println("CAUSE: " + e.getMessage());
+            System.exit(1);
         }
     }
 }
